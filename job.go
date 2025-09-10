1d643880a86334c218f3e58329b05a80081cd8ff
Mustafa Sadedil <mustafa.sadedil@authoritypartners.com>
2025-09-11 00:26:34 +0300

    Whitespace fix 2

--- job.go
+++ job.go
@@ -651,7 +651,7 @@ func (c *connection) connect(job *Job) error {
 				}
 				c.tokenExpirationTime = time.Now().Add(time.Hour)
 			}
-			
+
 			db, err := sqlx.Open("snowflake", c.snowflakeDSN)
 			if err != nil {
 				return fmt.Errorf("failed to open Snowflake connection: %w (host: %s)", err, c.host)
